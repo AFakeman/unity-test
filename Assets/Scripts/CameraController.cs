9522452186221b6c28d0eeeb4a9424cbd2af029d
Bombardir <panicfugur@gmail.com>
2019-01-23 22:13:54 +0300

    Ye

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 100f;
+    public Vector3 offset;
+
+    private void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothed = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothed;
+    }
+}
