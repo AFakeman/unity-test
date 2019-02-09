3c11070d23ca0ffafc39cdabfe6b4b3ce6e3ee2f
Alex Ollingworth <panicfugur@gmail.com>
2019-02-09 19:24:26 +0300

    Materials, day cycle, tiles

---
+++ Assets/Scripts/DayCycle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayCycle : MonoBehaviour
+{
+    public int dayDuration = 3000;
+    public float speed = 1f;
+    public float maxIntensity = 2.5f;
+
+    private Light _light;
+    private bool _sundown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _light = GetComponent<Light>();    
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (_light.intensity > 0 && _sundown)
+        {
+            float intensity = _light.intensity;
+            intensity -= 0.001f*speed;
+            _light.intensity = intensity;
+        }
+        else if (_light.intensity <= 2.5f && _sundown == false)
+        {
+            float intensity = _light.intensity;
+            intensity += 0.001f*speed;
+            _light.intensity = intensity;
+        }
+        else if (_light.intensity <= 0 || _light.intensity > maxIntensity)
+        {
+            _sundown = !_sundown;
+        }
+        
+    }
+}
