352748988d4c2f07aefd098d28b90317f71b4159
Anton Suslov <anton_suslov@me.com>
2020-01-06 00:27:27 +0300

    Made a generic UIBubble for displaying things
    * ItemBubble is now a descendant of UIBubble with sprite-specific stuff
    
    * UIBubble can be used as a generic display for your content, as long as
      the content borders are known
    
    * This is intended to use for mini-games

--- Assets/Scripts/ItemBubble.cs
+++ Assets/Scripts/ItemBubble.cs
@@ -6,17 +6,15 @@ using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ItemBubble : MonoBehaviour
+public class ItemBubble : UIBubble
 {
-    // Offsets are measured in pixels, pixel density
-    // is assumed to be the same as of the bubble itself.
-    public int marginLeft = 4;
-    public int marginRight = 3;
-    public int marginTop = 3;
-    public int marginBottom = 4;
-    public int spacingX = 2;
-    public string sortingLayerName = "UI";
+    // This is the sprite renderer priority so items would be
+    // rendered on top of the background
     public int sortingOrder = 1;
+    public string sortingLayerName = "UI";
+
+    // Spacing in pixels between items
+    public int spacingX = 2;
 
     public enum Style
     {
@@ -30,16 +28,9 @@ public class ItemBubble : MonoBehaviour
         public Sprite Sprite;
     }
 
-    private SpriteRenderer _bubble_sr;
-
     // Start is called before the first frame update.
     private void Start()
     {
-    }
-
-    private void Awake()
-    {
-        _bubble_sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -47,11 +38,8 @@ public class ItemBubble : MonoBehaviour
     {
     }
 
-    private float PixelsToUnits(float p)
-    {
-        return p / _bubble_sr.sprite.pixelsPerUnit;
-    }
-
+    // Default way to render sprites is with the solid style.
+    // See the next method comment to find out the constratints.
     public void RenderSprites(IEnumerable<Sprite> icons)
     {
         if (icons == null)
@@ -67,27 +55,33 @@ public class ItemBubble : MonoBehaviour
         RenderSprites(styledIcons);
     }
 
+    // Renders the provided collection of icons into the rect.
+    // They are assumed to have the same size (probably not enforced),
+    // and same pixel-to-unit ratio (enforced).
     public void RenderSprites(IEnumerable<StyledIcon> icons)
     {
-        ResetSprites();
+        Reset();
         if (icons == null || !icons.Any())
         {
+            // We hide the object if there is nothing to display.
             gameObject.SetActive(false);
             return;
         }
 
         gameObject.SetActive(true);
 
-        var newSize = BubbleSizeForIcons(icons);
-        _bubble_sr.size = newSize;
+        var iconContainer = new GameObject("IconContainer");
+        var contentSize = ContentSizeForIcons(icons);
 
-        float xPos = - (newSize.x * _bubble_sr.sprite.pixelsPerUnit / 2.0f) + marginLeft;
-        float yPos = (marginBottom - marginTop) / 2.0f;
+        float xPos = - (UnitsToPixels(contentSize.x) / 2.0f);
+        float yPos = 0;
 
         var i = 0;
         foreach (var icon in icons)
         {
-            var gameObjectForIcon = CreateGameObjectForIcon(icon, "Icon" + i);
+            var gameObjectForIcon = CreateGameObjectForIcon(
+                iconContainer.transform, icon, "Icon" + i);
+
             gameObjectForIcon.transform.localPosition = new Vector3(
                 PixelsToUnits(xPos + icon.Sprite.rect.width / 2),
                 PixelsToUnits(yPos),
@@ -97,14 +91,16 @@ public class ItemBubble : MonoBehaviour
             xPos += (int) icon.Sprite.rect.width + spacingX;
             ++i;
         }
+
+        SetContents(iconContainer, contentSize, true);
     }
 
-    private GameObject CreateGameObjectForIcon(StyledIcon icon, String name)
+    private GameObject CreateGameObjectForIcon(Transform parent, StyledIcon icon, String name)
     {
         var gameObjectForIcon = new GameObject(name);
         gameObjectForIcon.transform.parent = transform;
         gameObjectForIcon.transform.localScale = Vector3.one;
-        gameObjectForIcon.layer = 5;
+        gameObjectForIcon.layer = 5; // UI
 
         var spriteRenderer = gameObjectForIcon.AddComponent<SpriteRenderer>();
         spriteRenderer.sprite = icon.Sprite;
@@ -121,33 +117,29 @@ public class ItemBubble : MonoBehaviour
         return gameObjectForIcon;
     }
 
-    private Vector2 BubbleSizeForIcons(IEnumerable<StyledIcon> icons)
+    // Computes the size of rect occupied by icons in pixels.
+    // Assumes that they have the same pixel size as the bubble itself.
+    private Vector2 ContentSizeForIcons(IEnumerable<StyledIcon> icons)
     {
-        int width = marginLeft + marginRight - spacingX;
-        int maxSpriteHeight = 0;
+        // If there are N items, there are only N - 1 spacings.
+        int width = -spacingX;
+        int height = 0;
 
         foreach (var icon in icons)
         {
             var spriteRect = icon.Sprite.rect;
             width += (int) spriteRect.width + spacingX;
-            if (spriteRect.height  > maxSpriteHeight)
+
+            // I am not quite sure why this max thingy is needed, to be honest.
+            // Probably as not to hard-code any sprite size, but if we draw
+            // a 16x16 sprite and a 32x32 together it'll look fuck ugly.
+            if (spriteRect.height > height)
             {
-                maxSpriteHeight = (int) spriteRect.height;
+                height = (int) spriteRect.height;
             }
         }
 
-        int height = maxSpriteHeight + marginTop + marginBottom;
         return new Vector2(PixelsToUnits(width), PixelsToUnits(height));
 
-    }
-
-    public void ResetSprites()
-    {
-        for (int i = transform.childCount - 1; i >= 0; --i)
-         {
-             Transform child = transform.GetChild(i);
-             child.parent = null;
-             GameObject.Destroy(child.gameObject);
-         }
     }
 }
---
+++ Assets/Scripts/UIBubble.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBubble : MonoBehaviour
+{
+    // Offsets are measured in pixels.
+    public int marginLeft = 4;
+    public int marginRight = 3;
+    public int marginTop = 3;
+    public int marginBottom = 4;
+
+    private SpriteRenderer _bubble_sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void Awake()
+    {
+        _bubble_sr = GetComponent<SpriteRenderer>();
+    }
+
+    protected float PixelsToUnits(float p)
+    {
+        return p / _bubble_sr.sprite.pixelsPerUnit;
+    }
+
+    protected float UnitsToPixels(float p)
+    {
+        return p * _bubble_sr.sprite.pixelsPerUnit;
+    }
+
+    // Sets the root to the center of the canvas, and resizes for given size.
+    // If the parameter unpack is set to true, root object is considered
+    // just a container, and all the children will be transferred to
+    // UIBubble, destroying the root object. Used to reduce clutter.
+    public void SetContents(GameObject root, Vector2 contentSize, bool unpack=false)
+    {
+        root.transform.parent = transform;
+        root.transform.localPosition = GetCenterForContent();
+        _bubble_sr.size = BubbleSizeForContents(contentSize);
+        if (unpack) {
+            foreach (Transform obj in root.transform) {
+                obj.parent = transform;
+            }
+            root.transform.parent = null;
+            GameObject.Destroy(root);
+        }
+    }
+
+    // Returns the center of the canvas as a local position in units.
+    private Vector2 GetCenterForContent()
+    {
+        var size = _bubble_sr.size;
+        float x = (marginRight - marginLeft) / 2.0f;
+        float y = (marginBottom - marginTop) / 2.0f;
+        return new Vector2(x, y);
+    }
+
+    // Bubble size (in units) for contents including margins.
+    // Contents are assumed to be in units.
+    private Vector2 BubbleSizeForContents(Vector2 contentSize)
+    {
+        float width = PixelsToUnits(marginLeft + marginRight) + contentSize.x;
+        float height = PixelsToUnits(marginTop + marginBottom) + contentSize.y;
+
+        return new Vector2(width, height);
+    }
+
+    public void Reset()
+    {
+        // for loop instead of foreach since we are destroying children,
+        // and no guarantees on iteration are available in this case.
+        for (int i = transform.childCount - 1; i >= 0; --i)
+         {
+             Transform child = transform.GetChild(i);
+             child.parent = null;
+             GameObject.Destroy(child.gameObject);
+         }
+    }
+}
