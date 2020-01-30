e104dedd0cace733e04bdd7db143dce5598f16fd
Alex Ollingworth <panicfugur@gmail.com>
2020-01-30 18:56:03 +0300

    Added Test Scene

---
+++ Assets/Scripts/FunkyPile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FunkyPile : ItemSpendingItem
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    protected override void ItemsSpent(PlayerInteractionController Caller)
+    {
+        var item = new InventoryItem();
+        item.Name = "WoodenLog";
+        Caller.AddItem(item);
+        Destroy(this.gameObject);
+    }
+}
